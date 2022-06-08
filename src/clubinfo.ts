f9dd5c8692944477c06faedf63cd3459e21c34a5
holyCrawler <theholycrawler@gmail.com>
2022-06-08 11:30:59 +0100

    fixed some selectors

--- src/clubinfo.ts
+++ src/clubinfo.ts
@@ -1,31 +1,30 @@
 const parseClubInfo = () => {
-  const flagEl = document.querySelector("img[src*=flags_round]")!; // selecting the big round flag left of the club name
-  const lastActiveEl = document.querySelectorAll("td.maninfo")!; // selecting the last
+  const flagEl = document.querySelector("img[src*='images/flags_round']") as HTMLImageElement; // selecting the big round flag left of the club name
+  const lastActiveEl = document.querySelectorAll("td.maninfo") as NodeListOf<HTMLTableCellElement>; // selecting the last
 
-  const [teamNameEl, shortNameEl, StadiumEl, ratingEl, managerEl, , leagueEl, idEl] = document
-    .querySelector("div#content_main > div:last-child table")!
-    .querySelectorAll("td[class*=matches_row]:nth-child(2)"); // selecting the 2nd column of the "quick facts" table
-  const stadium = StadiumEl.textContent!.match(/(.*)\([0-9]*\/([0-9]*)\)/); // extracting the stadium name and capacity
+  const [teamNameEl, shortNameEl, StadiumEl, ratingEl, managerEl, , leagueEl, idEl] = (
+    document.querySelector("div#content_main > div:last-child table") as HTMLTableElement
+  ).querySelectorAll("td[class*=matches_row]:nth-child(2)") as NodeListOf<HTMLTableCellElement>; // selecting the 2nd column of the "quick facts" table
 
-  // this part just checks if the club has any achievements/trophies and maps them to an array
-  const trophiesEl = document.querySelector("div#trophy_0")
-    ? document.querySelector("div#trophy_0")!.parentElement!.querySelectorAll("img")!
-    : null;
-  const achivementsEl = document.querySelector("div.achieve")!.nextElementSibling!.querySelectorAll("img");
-  const trophies = trophiesEl ? Array.from(trophiesEl!).map((el) => el.getAttribute("title")!) : null;
-  const achivements = achivementsEl.length ? Array.from(achivementsEl).map((el) => el.getAttribute("title")!) : null;
+  const stadium = StadiumEl.textContent!.match(/(.*)\([0-9]*\/([0-9]*)\)/)!; // extracting the stadium name and capacity
+  const leagueLinkEl = leagueEl.firstElementChild as HTMLAnchorElement;
+  
+  const trophiesEl = document.querySelectorAll("img[src*='images/club/cups']") as NodeListOf<HTMLImageElement>;
+  const achivementsEl = document.querySelectorAll("img[src*='images/trophies']") as NodeListOf<HTMLImageElement>;
+  const trophies = Array.from(trophiesEl!).map((el) => el.getAttribute("title")!);
+  const achivements = Array.from(achivementsEl).map((el) => el.getAttribute("title")!);
 
   return {
     teamName: teamNameEl.textContent!.trim(),
     shortName: shortNameEl.textContent!.trim(),
     country: flagEl.getAttribute("title")!,
     lastActive: lastActiveEl[5].textContent!.trim(), // i didn't convert this to unix time because it's not a fixed format
-    stadium: { name: stadium![1].trim(), capacity: Number(stadium![2]) },
+    stadium: { name: stadium[1].trim(), capacity: Number(stadium[2]) },
     rating: Number(ratingEl.textContent!.replace(/\D+/g, "")),
     manager: managerEl.textContent!.trim(),
     league: {
-      name: leagueEl.firstElementChild!.textContent!,
-      url: (leagueEl.firstElementChild as HTMLAnchorElement).href,
+      name: leagueLinkEl.textContent!,
+      url: leagueLinkEl.href,
     },
     id: Number(idEl.textContent!.replace(/\D+/g, "")),
     trophies: trophies,
